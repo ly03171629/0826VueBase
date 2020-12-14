b5545578efa0cc14d62b4164226ede054ec1401d
liuyuan <1316425655@qq.com>
2020-12-14 17:24:47 +0800

    comments over

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,44 @@
 <template>
   <div>
-    <MyButton></MyButton>
+    <Header></Header>
+    <div class="container">
+      <Add :addComment="addComment"></Add>
+      <List :coms="comments" :deleteComment="deleteComment"></List>
+    </div>
   </div>
 </template>
 
 <script>
-import MyButton from '@/components/MyButton'
+import Header from '@/components/Header'
+import Add from '@/components/Add'
+import List from '@/components/List'
+
 export default {
-  name: '',
+  name: "",
   components:{
-    MyButton
+    Header,
+    Add,
+    List
+  },
+  data(){
+    return {
+      comments:[
+        {id:1,content:'vue牛逼',username:'赵丽颖'},
+        {id:2,content:'vue可以',username:'杨幂'},
+        {id:3,content:'vue不错',username:'戚薇'}
+      ]
+    }
+  },
+  methods:{
+    addComment(comment){
+      this.comments.unshift(comment)
+    },
+    deleteComment(index){
+      this.comments.splice(index,1)
+    }
   }
-}
+};
 </script>
 
 <style scoped>
-
 </style>
--- src/main.js
+++ src/main.js
@@ -1,35 +1,9 @@
-// /* eslint-disable no-unused-vars */
-// //局部禁用当前这个类型的错误（只禁用当前文件的这个类型的报错）
-import Vue from 'vue' //默认引入的就是runtime-only版本的，不带解析器
-// import Vue from 'vue/dist/vue.esm.js' //是我们自己找到的带解析器的版本
+import Vue from 'vue'
 import App from '@/App'
 
-// new Vue({
-//   el:'#app',
-//   components:{
-//     App,
-//   },
-//   template:'<App />'
-// })
+Vue.config.productionTip = false
 
-// You are using the runtime-only build of Vue where the template compiler is not available.
-//  Either pre-compile the templates into render functions, or use the compiler-included build.
-// 我们现在默认导入的Vue是一个 runtime-only版本的Vue，这个版本的Vue不带解析器
-
-
-// 这个使用render函数的 为什么就可以使用runtime-only版本的Vue
 new Vue({
   el:'#app',
-  render: h => h(App) //这个函数和上面我们自己写的 功能差不多
-                      // 1、定义并注册了App
-                      // 2、使用了App组件
-                      // 3、比上面的写法多干了一件事，就是寻找解析器的loader
+  render:h => h(App)
 })
-
-
-
-//以后我们要使用的是下面这种
-//下面的打包出来的项目体积小
-
-
-
